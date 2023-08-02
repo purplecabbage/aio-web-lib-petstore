e160b17464a161841ee8de06a24169d68cc40f52
Jesse MacFadyen <purplecabbage@gmail.com>
2023-08-02 13:05:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# aio-web-lib-template
+
+This template can be used for creating JavaScript libraries for use in App Builder front ends.
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* todo: implement this */
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+function Index () {
+  return 'Hello World'
+}
+
+module.exports = Index
